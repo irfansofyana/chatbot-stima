af9c463f9c477bcaa3cab839bd410033f702486d
a-johanes <antoniusjohanes99@gmail.com>
2019-04-18 02:23:12 +0700

    implement chatbot main

---
+++ chatbot.py
@@ -0,0 +1,90 @@
+from KMP import *
+from BM import *
+# from sortedcontainers import SortedDict
+
+CONFIDENCE_MIN_LEVEL = 90
+
+if __name__ == "__main__":
+    # pertanyaan wajib
+    data = open("wajib_clear.txt",'r')
+    db = []
+    pos = 0
+    # read database
+    for line in data:
+        line = line.rstrip("\n")
+        if(pos%2==0):
+            quest = line
+        else:
+            ans = line
+            db.append((quest,ans))
+        pos+=1
+    data.close()
+    # stopword
+    data = open("stopword.txt",'r')
+    stop = []
+    for line in data:
+        stop.append(line.rstrip("\n"))
+    data.close()
+    # stop = StopWordRemoverFactory().get_stop_words()
+    # print(stop)
+    
+    pattern = ""
+    quitKey = "q"
+    while pattern!=quitKey:
+        pattern = input("Pertanyaan: ").lower()
+        if(not pattern.endswith("?") and pattern != quitKey):
+            print("Input bukan pertanyaan")
+        elif pattern == quitKey:
+            print("Terima Kasih!")    
+        else:
+            # asking a question
+            pattern = pattern[:-1]
+            # clear stopword
+            split = pattern.split(' ')
+            result = [kata for kata in split if kata not in stop]
+            pattern = ' '.join(result)
+            if (len(pattern)==0):
+                print("Maaf saya tidak mengerti petanyaan anda")
+            else:
+                # print(pattern)
+                confi = 0
+                # found = False
+                rest = []
+                cont = []
+                for quest, ans in db:
+                    confi = bmFindPercentage(quest,pattern)
+                    # print(confi)
+                    if(confi>=CONFIDENCE_MIN_LEVEL):
+                        # print(ans)
+                        # found = True
+                        # break
+                        cont.append((confi,(quest,ans)))
+                    else:
+                        rest.append((confi,(quest,ans)))
+                if(len(cont)>0):
+                    # ada minimal 1 yang pass
+                    cont.sort(key=lambda tuple: tuple[1])
+                    # print(cont)
+                    if(len(cont)>1):
+                        if(cont[0][0]!=cont[1][0]):
+                            print(cont[0][1][1])
+                            continue
+                        else:
+                            top3 = cont[:3]
+                            print("Apakah yang anda maksud:")
+                            for el in top3:
+                                print(el[1][0]+'?')
+                    else:
+                        print(cont[0][1][1])
+                        continue
+                if(len(rest)>0):
+                    # rest minimal 1
+                    rest.sort(key=lambda tuple: tuple[1])
+                    top3 = rest[:3]
+                    print("Apakah yang anda maksud:")
+                    for el in top3:
+                        print(el[1][0]+'?')
+                else:
+                    print("Maaf saya tidak memiliki jawaban untuk pertanyaan anda")
+    # print(stop)
+
