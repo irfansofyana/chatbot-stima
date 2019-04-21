c2508e4f2847fa466ca625f8dd98614ac520520d
Ahmad Rizal Alifio <poisaja@gmail.com>
2019-04-21 08:08:40 +0700

    debug

--- post.php
+++ post.php
@@ -4,7 +4,7 @@ if(isset($_SESSION['name'])){
     $text = $_POST['text'];
 
     $fp = fopen("log.html", 'a');
-    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>"."User"."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
+    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION ['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
     fclose($fp);
 }
 ?>
