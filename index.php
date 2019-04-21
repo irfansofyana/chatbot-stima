7e7bfe46726c95a1f2245fec6db9d4745f2ed922
Ahmad Rizal Alifio <poisaja@gmail.com>
2019-04-21 07:54:17 +0700

    Debug index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//awal mulai
 session_start ();
 function loginForm() {
     echo '
@@ -13,17 +14,21 @@ function loginForm() {
    ';
 }
 
+//aksi enter
 if (isset ( $_POST ['enter'] )) {
     if ($_POST ['name'] != "") {
         $_SESSION ['name'] = stripslashes ( htmlspecialchars ( $_POST ['name'] ) );
         $fp = fopen ( "log.html", 'a' );
         fwrite ( $fp, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has joined the chat session.</i><br></div>" );
+        $initbotmsg = ("Halo, ".$_POST ['name']."! apa yang ingin kau tanyakan?");
+        fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".'io'."</b>: ".stripslashes(htmlspecialchars($initbotmsg))."<br></div>");
         fclose ( $fp );
     } else {
         echo '<span class="error">Please type in a name</span>';
     }
 }
 
+//aksi logout
 if (isset ( $_GET ['logout'] )) {
 
     // Simple exit message
@@ -39,13 +44,71 @@ if (isset ( $_GET ['logout'] )) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+
+<html lang="en" class="h-100">
+    <title>Chatbot io</title>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="mainView.css" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="style.css" />
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
+  </head>
+  <body class="d-flex flex-column h-100">
+    <header>
+  <!-- Fixed navbar -->
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="#">Chatbot io</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="profile.php">About Us<span class="sr-only">(current)</span></a>
+        </li>
+        <!--
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+        -->
+      </ul>
+        <!--
+        <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        -->
+    </div>
+  </nav>
+</header>
+
 <style>
-body {
-    font: 12px arial;
-    color: #222;
-    text-align: center;
-    padding: 35px;
-}
 
 form,p,span {
     margin: 0;
@@ -71,6 +134,7 @@ a:hover {
     background: #EBF4FB;
     width: 504px;
     border: 1px solid #ACD8F0;
+    text-align: center;
 }
 
 #loginform {
@@ -122,14 +186,26 @@ a:hover {
     margin: 0 0 2px 0;
 }
 </style>
-<title>Chat - Customer Module</title>
 </head>
+
 <body>
+
+<!-- Begin page content -->
+<main role="main" class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">Say Hello to io!</h1>
+    <!--<p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>main &gt; .container</code>.</p>-->
+    <p class="lead">This chatbot was made by truly innovative techniques of copying bootstrap and decent(ish) development capabilites. Enjoy!</p>
+        <!--<p>Back to <a href="/docs/4.3/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>-->
+  </div>
+
+
     <?php
     if (! isset ( $_SESSION ['name'] )) {
         loginForm ();
     } else {
         ?>
+<!-- Chatbox -->
 <div id="wrapper">
         <div id="menu">
             <p class="welcome">
@@ -210,5 +286,13 @@ setInterval (loadLog, 1000);
         src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
     <script type="text/javascript">
 </script>
+
+</main>
+
+<footer class="footer mt-auto py-3">
+  <div class="container">
+    <span class="text-muted">Copyright of Chatbot Group</span>
+  </div>
+</footer>
 </body>
 </html>
