36def740aed2135c7ab53b77db2f6673c9042484
Irfan Sofyana Putra <irfansofyana0305@gmail.com>
2019-04-22 00:43:54 +0700

    add sinonim

---
+++ ChangeQuestion.py
@@ -0,0 +1,48 @@
+f = open("sinonim.txt", "r")
+hashing = {}
+
+def generate():
+    f1 = f.readlines()
+    get = ""
+    for string in f1:
+        first = True
+        key = ""
+        get = ""
+        j = 0
+        while (j < len(string)-1):
+            if (string[j] == ','):
+                if (len(get) > 0):
+                    if (first):
+                        first = False
+                        key = get
+                    # print(get, key)
+                    hashing[get] = key
+                    get = "" 
+                k = j+1
+                while (k < len(string) and string[k] == ' '):
+                    k += 1
+                j = k
+            else:
+                get += string[j]
+                j+=1
+        hashing[get] = key
+
+def change(text):
+    res = ""
+    first = True
+    for i in text.split():
+        if (first):
+            first = False
+        else:
+            res += " "
+        if (i in hashing):
+            res += hashing[i]
+        else:
+            res += i
+    return res
+
+generate()
+x = input()
+print(change(x))
+                
+
