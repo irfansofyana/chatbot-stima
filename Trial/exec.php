13a9f4d3a513fc5e51c5b86e7cf1c7f355a46ebf
Irfan Sofyana Putra <irfansofyana0305@gmail.com>
2019-04-22 09:16:01 +0700

    fix bug in skenario

--- Trial/exec.php
+++ Trial/exec.php
@@ -1,14 +1,4 @@
 <?php
-<<<<<<< HEAD
-session_start();
-if(isset($_POST['text'])){
-    $resbotmsg = exec("python chatbot.py "+$_POST['text']);
-    $fp = fopen("log.html", 'a');
-    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".'io'."</b>: ".stripslashes(htmlspecialchars($resbotmsg))."<br></div>");
-    fclose($fp);
-}
-?>
-=======
 if(isset($_POST['usermsg'])){
   // $resbotmsg = exec("python3 chatbot.py "+$_POST['usermsg']);
   exec("python chatbot.py ".$_POST['usermsg'],$temp);
@@ -22,4 +12,3 @@ $fp = fopen("log.html", 'a');
 fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".'io'."</b>: ".stripslashes(htmlspecialchars($resbotmsg))."<br></div>");
 fclose($fp);
 ?>
->>>>>>> 35d212c9cd644654569bcc174f27429cc1f47094
